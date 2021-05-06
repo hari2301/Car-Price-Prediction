5c9e99826904acc4635b3dff4bd833a4d11d7fb7
Hari_vicky <46350562+hari2301@users.noreply.github.com>
2021-05-06 16:33:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('reg_car_pred.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Kms_Driven2=np.log(Kms_Driven)
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        Year=2021-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        
+        prediction=model.predict([[Present_Price,Kms_Driven2,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual,Owner,Year]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
